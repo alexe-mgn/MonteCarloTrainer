d9e1796800ad0080784493699187f13fa36d7b14
alexe-mgn <alexe-mgn@mail.ru>
2023-08-08 05:06:35 +0300

    +plotly +plot page +webView integration

--- client/gui/TaskWidget.py
+++ client/gui/TaskWidget.py
@@ -1,6 +1,7 @@
 from PySide6.QtWidgets import QWidget
 
 from client.gui.UI.TaskWidget import Ui_TaskWidget
+from client.gui.plot.PlotController import PlotController
 
 
 class TaskWidget(QWidget, Ui_TaskWidget):
@@ -8,6 +9,8 @@ class TaskWidget(QWidget, Ui_TaskWidget):
     def __init__(self):
         super().__init__()
         self.setupUi()
+
+        self._plot_controller = PlotController(self.viewPlot)
 
     def setupUi(self, target=None):
         super().setupUi(self if target is None else target)
--- client/main.py
+++ client/main.py
@@ -1,23 +1,47 @@
-def install():
-    from client.utils import STATE
-    if not STATE.FROZEN:
-        from client.gui.ui_utils import compile_ui
+import traceback
 
+
+def install(update=False):
+    import logging
+    log = logging.getLogger('meta')
+
+    from client.utils import STATE, PATH
+
+    if not STATE.FROZEN:
+        log.warning('Unbundled runtime, compiling ui.')
+        from client.gui.ui_utils import compile_ui
         compile_ui()
+
+    import os
+    if not os.path.isfile(PATH.PLOTLY_JS):
+        log.warning(f'Plotly JS file not found at "{PATH.PLOTLY_JS}".')
+
+    import plotly.offline
+    if not os.path.isfile(PATH.PLOTLY_JS) or update:
+        log.warning(f'Downloading Plotly JS at "{PATH.PLOTLY_JS}".')
+        with open(PATH.PLOTLY_JS, mode='w', encoding='UTF-8') as f:
+            f.write(plotly.offline.get_plotlyjs())
 
 
 def run():
-    install()
+    try:
+        install()
 
-    from PySide6.QtWidgets import QApplication
-    from client.gui.TaskWidget import TaskWidget
+        from PySide6.QtWidgets import QApplication
+        from client.gui.TaskWidget import TaskWidget
 
-    app = QApplication([])
+        app = QApplication([])
 
-    mw = TaskWidget()
-    mw.show()
+        mw = TaskWidget()
+        mw.show()
 
-    app.exec()
+        app.exec()
+    except Exception:
+        traceback.print_exc()
+        from client.utils import PATH
+        for k in dir(PATH):
+            print(f'{k}\t=\t{getattr(PATH, k)}')
+        input()
 
 
 if __name__ == '__main__':

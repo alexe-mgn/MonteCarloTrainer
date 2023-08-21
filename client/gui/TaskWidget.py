25f051f98edf731aa29510cb4d7cadb5018869d6
alexe-mgn <alexe-mgn@mail.ru>
2023-08-21 03:47:17 +0300

    Initial ui-task bindings, separate TaskSession, improved typehints, additional TaskSession getters.

--- client/gui/TaskWidget.py
+++ client/gui/TaskWidget.py
@@ -1,8 +1,12 @@
+import random
+
 from PySide6.QtWidgets import QWidget
 
-from client.exceptions import AppError
 from common.task.utils import STEP
-from client.task.Task import Task, TaskSession
+
+from client.exceptions import AppError
+from client.task.Task import Task
+from client.task.TaskSession import TaskSession
 
 from client.gui.UI.TaskWidget import Ui_TaskWidget
 
@@ -41,16 +45,24 @@ class TaskWidget(QWidget, Ui_TaskWidget):
 
         self._task_session: TaskSession | None = None
 
+        self._connect_ui()
+
     def setupUi(self, target=None):
         super().setupUi(self if target is None else target)
         for w in (self.buttonRectComplete, self.buttonPointsComplete, self.buttonIntComplete):
             w.clicked.connect(self.next_step)
+
+    def _connect_ui(self):
+        self.buttonRectComplete.clicked.connect(self._rect_complete)
+        self.buttonPointsComplete.clicked.connect(self._points_complete)
+        self.buttonIntComplete.clicked.connect(self._integral_complete)
 
     def task(self):
         return self._task_session.task
 
     def set_task(self, task: Task):
         self._task_session = TaskSession(task)
+        self._task_session.start()
         for step in self._STEPS:
             is_current = step == self._task_session.step
             self._step_widgets[step].setEnabled(is_current)
@@ -66,3 +78,37 @@ class TaskWidget(QWidget, Ui_TaskWidget):
             self._step_widgets[step].setEnabled(is_current)
             # self._spoilers[step].set_expanded(current)
             self._spoilers[step].expand(is_current)
+
+    def _rect_complete(self):
+        self._task_session.set_int_x((self.inputRectX1.value(), self.inputRectX2.value()))
+        self._task_session.set_int_y((self.inputRectY1.value(), self.inputRectY2.value()))
+        self.next_step()
+
+    def _points_generate(self):
+        x = self._task_session.int_x()
+        y = self._task_session.int_y()
+        p = (x[0] + random.random() * (x[1] - x[0]), y[0] + random.random() * (y[1] - y[0]))
+        self._task_session.generate_point(p)
+
+        self.viewPointsX.setText(str(p[0]))
+        self.viewPointsY.setText(str(p[1]))
+        self.viewPointsFY.setText(str(self._task_session.task.f(p[0])))
+
+    def _points_count(self):
+        hits = self._task_session.point_hits()
+        self.viewPointsMiss.display(sum(not e for e in hits))
+        self.viewPointsHit.display(sum(bool(e) for e in hits))
+
+    def _points_miss(self):
+        self._task_session.count_point(False)
+        self._points_count()
+
+    def _points_hit(self):
+        self._task_session.count_point(True)
+        self._points_count()
+
+    def _points_complete(self):
+        self.next_step()
+
+    def _integral_complete(self):
+        self.next_step()
---
+++ client/task/TaskSession.py
@@ -0,0 +1,152 @@
+import functools
+import time
+
+from common.task.utils import STEP, ERROR, ERRORS
+from client.task.exceptions import TaskError
+from client.task.Task import Task, Interval
+
+Point = tuple[float, float]
+
+
+class TaskSession:
+
+    def __init__(self, task: Task):
+        self._task = task
+
+        self._step = STEP.START
+
+        self._time = {}
+
+        self._int_x: Interval | None = None
+        self._int_y: Interval | None = None
+
+        self._points = []
+        self._points_hit = []
+        self._point_counted = True
+
+    @property
+    def task(self):
+        return self._task
+
+    @property
+    def step(self):
+        return self._step
+
+    def raise_for_step(self, step: STEP, code: ERROR):
+        if self._step is not step:
+            raise TaskError(self._step, code)
+
+    @staticmethod
+    def check_step(step: STEP, error: ERROR):
+        def decorator(func):
+            @functools.wraps(func)
+            def wrapper(self, *args, **kwargs):
+                self.raise_for_step(step, error)
+                return func(self, *args, **kwargs)
+
+            return wrapper
+
+        return decorator
+
+    def start(self):
+        if self._step is not STEP.START:
+            raise UserWarning(f"{self} is already started at {self._time.get('start', None)}.")
+        else:
+            self._step = STEP.RECT
+            self._time['start'] = time.time()
+
+    def end(self):
+        if self._step is STEP.END:
+            raise UserWarning(f"{self} is already finished at {self._time.get('end', None)}")
+        else:
+            self._step = STEP.END
+            self._time['end'] = time.time()
+
+    def next_step(self):
+        error = TaskError(self._step)
+        nxt = None
+        match self._step:
+            case STEP.START:
+                self.start()
+            case STEP.RECT:  # TODO checks
+                if self._int_x is None:
+                    error |= ERRORS.RECT.X_0 | ERRORS.RECT.X_1
+                if self._int_y is None:
+                    error |= ERRORS.RECT.Y_0 | ERRORS.RECT.Y_1
+                nxt = STEP.POINTS
+            case STEP.POINTS:
+                nxt = STEP.INTEGRAL
+            case STEP.INTEGRAL:
+                nxt = STEP.ERROR
+            case STEP.ERROR:
+                self.end()
+            case _:
+                raise RuntimeError(f"{self} can't switch from step {self._step}")
+        if error:
+            raise error
+        elif nxt is not None:
+            self._step = nxt
+
+    def int_x(self) -> Interval | None:
+        return self._int_x
+
+    @check_step(STEP.RECT, ERRORS.RECT.WRONG_STEP)
+    def set_int_x(self, interval: Interval):
+        error = TaskError(self._step)
+        if interval[0] != self._task.interval[0]:
+            error |= ERRORS.RECT.X_0
+        if interval[1] != self._task.interval[1]:
+            error |= ERRORS.RECT.X_1
+        if error:
+            raise error
+        else:
+            self._int_x = tuple(interval)
+
+    def int_y(self) -> Interval | None:
+        return self._int_y
+
+    @check_step(STEP.RECT, ERRORS.RECT.WRONG_STEP)
+    def set_int_y(self, interval: Interval):
+        error = TaskError(self._step)
+        if interval[0] != self._task.interval[0]:  # TODO y checks
+            error |= ERRORS.RECT.Y_0
+        if interval[1] != self._task.interval[1]:
+            error |= ERRORS.RECT.Y_1
+        if error:
+            raise error
+        else:
+            self._int_y = tuple(interval)
+
+    def points(self) -> list[Point]:
+        return list(self._points)
+
+    def point_hits(self) -> list[bool]:
+        return list(self._points_hit)
+
+    @check_step(STEP.POINTS, ERRORS.POINTS.WRONG_STEP)
+    def generate_point(self, p: Point):
+        if not self._point_counted:
+            raise TaskError(self._step, ERRORS.POINTS.GENERATE_BEFORE_COUNT)
+        else:
+            if not (self._int_x[0] <= p[0] <= self._int_x[1] and self._int_y[0] <= p[1] <= self._int_y[1]):
+                raise TaskError(self._step, ERRORS.POINTS.POINT)
+            else:
+                self._points.append(tuple(p))
+                self._point_counted = False
+
+    @check_step(STEP.POINTS, ERRORS.POINTS.WRONG_STEP)
+    def count_point(self, hit: bool):
+        if self._point_counted:
+            raise TaskError(self._step, ERRORS.POINTS.COUNT_BEFORE_GENERATE)
+        else:
+            p = self._points[-1]
+            y_f = self._task.f(p[0])
+            hit_real = p[1] <= y_f
+            if hit != hit_real:
+                raise TaskError(self._step, ERRORS.POINTS.COUNT)
+            else:
+                self._points_hit.append(hit_real)
+                self._point_counted = True
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self._task}, step={self._step})"
